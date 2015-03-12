ebbf2bf47c345465c0142aa3a600cc1cfe91d67b
BonnieLiu <liudanhjr@126.com>
2015-03-12 11:46:37 +0800

    Create test_suit.py

---
+++ test_suit.py
@@ -0,0 +1,7 @@
+#coding=utf-8
+import unittest
+import HTMLTestRunner
+from cases.lijigou_case import LijiGou
+from cases.order_cart_case import OrderCart
+from cases.cancel_order_case import CancelOrder
+from cases.cart_delete_case import CartDelete
